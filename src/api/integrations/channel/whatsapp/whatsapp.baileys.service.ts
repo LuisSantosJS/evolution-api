140afcf30e5c816a0709547fe4b8349ca5d2b993
luissantosjs <dasilvasantosluisfelipe@gmail.com>
2025-09-04 22:25:43 +0100

    up

--- src/api/integrations/channel/whatsapp/whatsapp.baileys.service.ts
+++ src/api/integrations/channel/whatsapp/whatsapp.baileys.service.ts
@@ -1467,8 +1467,18 @@ export class BaileysStartupService extends ChannelStartupService {
           if (update.message === null && update.status === undefined) {
             this.sendDataWebhook(Events.MESSAGES_DELETE, key);
 
-            if (this.configService.get<Database>('DATABASE').SAVE_DATA.MESSAGE_UPDATE)
-              await this.prismaRepository.messageUpdate.create({ data: message });
+            if (this.configService.get<Database>('DATABASE').SAVE_DATA.MESSAGE_UPDATE) {
+              if (message.messageId) {
+                await this.prismaRepository.messageUpdate.create({ 
+                  data: {
+                    ...message,
+                    Message: {
+                      connect: { id: message.messageId }
+                    }
+                  }
+                });
+              }
+            }
 
             if (this.configService.get<Chatwoot>('CHATWOOT').ENABLED && this.localChatwoot?.enabled) {
               this.chatwootService.eventWhatsapp(
@@ -1514,8 +1524,18 @@ export class BaileysStartupService extends ChannelStartupService {
 
           this.sendDataWebhook(Events.MESSAGES_UPDATE, message);
 
-          if (this.configService.get<Database>('DATABASE').SAVE_DATA.MESSAGE_UPDATE)
-            await this.prismaRepository.messageUpdate.create({ data: message });
+          if (this.configService.get<Database>('DATABASE').SAVE_DATA.MESSAGE_UPDATE) {
+            if (message.messageId) {
+              await this.prismaRepository.messageUpdate.create({ 
+                data: {
+                  ...message,
+                  Message: {
+                    connect: { id: message.messageId }
+                  }
+                }
+              });
+            }
+          }
 
           const existingChat = await this.prismaRepository.chat.findFirst({
             where: { instanceId: this.instanceId, remoteJid: message.remoteJid },
